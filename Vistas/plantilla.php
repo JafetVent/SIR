bb136b0e15cf46e8837834594df73669f9c61085
Pass-Jafet <16240626@leon.tecnm.mx>
2021-03-05 16:05:41 -0600

    agregue menu desplegable

--- Vistas/plantilla.php
+++ Vistas/plantilla.php
@@ -40,9 +40,74 @@ session_start();
   </div>
     </div>
 
+
+    <!-- MENU DESPLEGABLE -->
+
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #0275d8;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #292b2c;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: ##292b2c;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 35px;}
+  .sidenav a {font-size: 18px;}
+}
+</style>
+</head>
+
+
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  
+  <a href="#"><i class="fas fa-user"></i>Perfil</a>
+  <a href="#">Inicio</a>
+  <a href="#">Facturas</a>
+  <a href="#">Partes</a>
+  <a href="#">Cerrar Sesión</a>
+
+</div>
+
+<div id="main" class="container-fluid">
+  <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Menú</span>
+                         
+                         <!--------------CONTENIDO-------->
+              
 <!--------------CONTENIDO-------->
-       <div class="container-fluid">
-                    <div class="container py-5">       
+    
                            <?php
 
                 #ISSET: isset() determina su una variable esta definida y no es NULL
@@ -71,7 +136,21 @@ session_start();
 
                         ?>  
    </div>
- </div>
-              
+
+
+
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+</script>
+
+    
             </body>
 </html>
